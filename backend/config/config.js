030818c2b401cff634d2ef3e600d0827caa1c22a
Tarık Buğra Ayvazoğlu <tbafb1907@gmail.com>
2025-12-21 20:14:37 +0300

    Production veritabani ayari

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,25 +1,47 @@
-require('dotenv').config(); // .env dosyasını okur
+require('dotenv').config(); // Bunu en üste ekle
 
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: "postgres"
-  },
-  test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASSWORD, // Kendi yerel şifren
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     dialect: "postgres"
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: "postgres"
+    use_env_variable: process.env.DB_DATABASE, // Render bu değişkeni verecek
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
+
+// require('dotenv').config(); // .env dosyasını okur
+
+// module.exports = {
+//   development: {
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: 0,
+//     host: process.env.DB_HOST,
+//     dialect: "postgres"
+//   },
+//   test: {
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+//     host: process.env.DB_HOST,
+//     dialect: "postgres"
+//   },
+//   production: {
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+//     host: process.env.DB_HOST,
+//     dialect: "postgres"
+//   }
+// };
